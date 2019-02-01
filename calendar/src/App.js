cbac7fa589bbbaa499a3cb130a54d85d97f2b33e
Esko Salminen <esko.salminen@gmail.com>
2019-02-01 21:35:39 +0200

    Initial commit 2

---
+++ calendar/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Calendar from './components/calendar'
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div id="logo">
+        <span className="icon">date_range</span>
+        <span>
+          raskaus<b>diabetes</b>
+        </span>
+        </div>
+        </header>
+        <main>
+          <Calendar />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
